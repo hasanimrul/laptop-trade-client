5142efac02ad103afedc14aa95f67588e65e2e68
Md. Imrul Hasan <mohammadhasan.imrul@gmail.com>
2022-11-28 16:12:54 +0600

    product shown as per sale status

--- src/pages/AllProducts/AllProductsCard.js
+++ src/pages/AllProducts/AllProductsCard.js
@@ -11,27 +11,34 @@ const AllProductsCard = ({ product, setAllProducts }) => {
                 setRole(data)
             })
     }, [user])
-    const { productName, categoryName, price, description, year, condition, number, location, image } = product;
+    const { productName, categoryName, price, description, year, condition, number, location, image, paid } = product;
     return (
 
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={image} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    {productName}
-                    <div className="badge badge-secondary">{condition}</div>
-                </h2>
-                <p>{description}</p>
-                <h2>Purchase year: {year}</h2>
-                <h2>Contact: {number}</h2>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">${price}</div>
-                    <div className="badge badge-outline">{location}</div>
-                </div>
-                {role === "Buyer" ? <label htmlFor="booking-modal" onClick={() => setAllProducts(product)} className="btn">Book now</label>
+        <div>
+            {
+                paid === true ?
+                    <div className='hidden'></div>
                     :
-                    <div className="badge badge-outline bg-red-500 text-white p-5 mx-auto">You can't order this item</div>}
-            </div>
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <figure><img src={image} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                {productName}
+                                <div className="badge badge-secondary">{condition}</div>
+                            </h2>
+                            <p>{description}</p>
+                            <h2>Purchase year: {year}</h2>
+                            <h2>Contact: {number}</h2>
+                            <div className="card-actions justify-end">
+                                <div className="badge badge-outline">${price}</div>
+                                <div className="badge badge-outline">{location}</div>
+                            </div>
+                            {role === "Buyer" ? <label htmlFor="booking-modal" onClick={() => setAllProducts(product)} className="btn">Book now</label>
+                                :
+                                <div className="badge badge-outline bg-red-500 text-white p-5 mx-auto">You can't order this item</div>}
+                        </div>
+                    </div>
+            }
         </div>
 
     );
